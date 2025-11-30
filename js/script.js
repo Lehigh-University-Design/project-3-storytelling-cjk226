6b8705620cc0835c6d056c45acb98af150e2fd8f
cjk226 <cjk226@lehigh.edu>
2025-11-30 12:44:51 -0500

    11/30
    work

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 // Start JS script code, feel free to not use this or remove it
+document.querySelectorAll('#ending .choice-btn').forEach(button => {
+    button.addEventListener('click', function () {
+        const outcomeId = this.getAttribute('href');
+        
+        // Hide all outcomes first
+        document.querySelectorAll('#ending .outcome')
+            .forEach(o => o.classList.add('hidden'));
+
+        // Show the one selected
+        document.querySelector(outcomeId).classList.remove('hidden');
+    });
+});
+
 
 console.log("Hello, World!");
